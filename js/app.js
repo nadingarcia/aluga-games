b0739452aaf4f1bd42059fbbfaadc2f326698271
nadingarcia <159864236+nadingarcia@users.noreply.github.com>
2024-02-19 17:12:47 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let jogosAlugados = 1;
+
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.innerHTML = 'Alugar';
+        jogosAlugados --; 
+    }
+  }  else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.innerHTML = 'Devolver';
+        jogosAlugados ++;
+    }
+    console.log(jogosAlugados);
+} 
